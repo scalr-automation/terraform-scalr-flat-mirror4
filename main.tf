2e5bfc8d438c145c9802a76ab853a1f55f2db010
Glen Bell <auto-test@scalr.lo>
2026-06-22 17:10:37 +0300

    add random pet to configuration to branch 'ahead-cut-professional'

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
-resource "random_pet" "run_from_vcs" {
-  keepers = {
-    timestamp = timestamp()
-  }
+resource "terraform_data" "resource_to_update" {
+  input = "v2"
+}
+
+resource "terraform_data" "resource_to_add" {
+  input = "new"
 }
